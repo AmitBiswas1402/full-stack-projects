e0bd3cdc047dfc12d631889513654b34f59f6f32
Amit Biswas <amit.142biswas@gmail.com>
2024-03-22 18:20:17 +0530

    leaving because neon db is not allowing more postgres

--- search-engine/src/app/search/page.tsx
+++ search-engine/src/app/search/page.tsx
@@ -1,4 +1,19 @@
-const page = () => {
+import { redirect } from "next/navigation"
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+
+  }
+}
+
+const page = ({searchParams}: PageProps) => {
+  const query = searchParams.query 
+
+  if(Array.isArray(query) || !query){
+    return redirect('/')       
+  }
+
   return (
     <p>
         
