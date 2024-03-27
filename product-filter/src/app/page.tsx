d9202213c02b1e94bbe0b63829eade4f313c9fea
Amit Biswas <amit.142biswas@gmail.com>
2024-03-27 21:22:25 +0530

    shadcn ui and other initiated

---
+++ product-filter/src/app/page.tsx
@@ -0,0 +1,24 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function Home() {
+  return (
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+          High-Quality cotton Selection
+        </h1>
+
+        <div className="flex items-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+              
+            </DropdownMenuTrigger>
+          </DropdownMenu>
+        </div>
+      </div>
+    </main>
+  );
+}
