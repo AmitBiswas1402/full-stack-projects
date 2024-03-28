b32608e938e95b3a9cd2d30590d6ac12268f5131
Amit Biswas <amit.142biswas@gmail.com>
2024-03-28 19:34:16 +0530

    filter and upstash db added

--- product-filter/src/app/page.tsx
+++ product-filter/src/app/page.tsx
@@ -1,9 +1,26 @@
+"use client"
+
 import {
   DropdownMenu,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu";
+import { ChevronDown, Filter } from "lucide-react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+
+const SORT_OPTIONS = [
+  { name: 'None', value: 'none' },
+  { name: 'Price: Low to High', value: 'price-asc' },
+  { name: 'Price: High to Low', value: 'price-desc' },
+] as const
 
 export default function Home() {
+  const [filter, setFilter] = useState({
+    sort: 'none',
+  })
+  console.log(filter);
+  
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
@@ -14,9 +31,28 @@ export default function Home() {
         <div className="flex items-center">
           <DropdownMenu>
             <DropdownMenuTrigger className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-              
+              Sort    
+              <ChevronDown className="mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />          
             </DropdownMenuTrigger>
+
+            <DropdownMenuContent align='end'>
+              {SORT_OPTIONS.map((options) =>
+              <button key={options.name} className={cn('text-left w-full block px-4 py-2 text-sm', {
+                'text-gray-900 bg-gray-100': options.value === filter.sort,
+                'text-gray-500': options.value !== filter.sort,
+              })} onClick={() => {
+                setFilter((prev) => ({
+                  ...prev,
+                  sort: options.value
+                }))
+              }}>{options.name}</button>
+              )}              
+            </DropdownMenuContent>
           </DropdownMenu>
+
+          <button className='-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden'>
+            <Filter className='h-5 w-5' />
+          </button>
         </div>
       </div>
     </main>
